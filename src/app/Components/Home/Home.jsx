cb72ba6846f3a1b4c903e3f2b77ea4ecd11462c7
Robert van Steen <studio@rovansteen.nl>
2016-06-01 12:14:25 +0200

    Remove devtools

--- src/app/Components/Home/Home.jsx
+++ src/app/Components/Home/Home.jsx
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
-import DevTools from 'mobx-react-devtools';
 import StoryList from 'Components/StoryList/StoryList';
 
 class Home extends Component {
 	render() {
 		return (
-			<div>
-				<DevTools />
-				<StoryList />
-			</div>
+			<StoryList />
 		);
 	}
 }
