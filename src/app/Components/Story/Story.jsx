e0b314d0a49873be4c36dd14591ab0dee9759d51
Robert van Steen <studio@rovansteen.nl>
2016-06-01 12:16:02 +0200

    Add styling for header & body

--- src/app/Components/Story/Story.jsx
+++ src/app/Components/Story/Story.jsx
@@ -4,15 +4,31 @@ import React, { Component, PropTypes } from 'react';
 
 class Story extends Component {
 
+	/**
+	 * The type of the props the component expects.
+	 *
+	 * @type {Object}
+	 */
 	static propTypes = {
 		active: PropTypes.bool.isRequired,
 		story: PropTypes.object.isRequired,
 	}
 
+	/**
+	 * Invoked when the component is mounted.
+	 *
+	 * @return {void}
+	 */
 	componentDidMount() {
 		window.addEventListener('scroll', _.throttle(() => this.onScroll(), 200, { trailing: true }));
 	}
 
+	/**
+	 * Invoked when the component will receive props.
+	 *
+	 * @param  {Object} nextProps
+	 * @return {void}
+	 */
 	componentWillReceiveProps(nextProps) {
 		if (nextProps.active) {
 			this.refs.element.style.transform = 'translateY(0)';
@@ -20,12 +36,32 @@ class Story extends Component {
 		}
 	}
 
+	/**
+	 * Invoked when there is a scroll event.
+	 * This function is throttled to improve performance, see ComponentDidMount.
+	 *
+	 * @return {void}
+	 */
 	onScroll() {
 		const scrolled = window.scrollY;
 		if (!this.props.active) {
 			this.refs.element.style.transform = `translateY(${scrolled}px)`;
 			this.refs.element.style.height = '100vh';
 		}
+	}
+
+	/**
+	 * Parse the body of the story to set the CSS modules classes on the elements.
+	 *
+	 * @param  {String} body
+	 * @return {String}
+	 */
+	parseBody(body) {
+		let parsedBody = body;
+		parsedBody = parsedBody.replace(/<p(.*?)>/g, `<p class="${styles.body__paragraph}">`);
+		parsedBody = parsedBody.replace(/<p(.*?)>(\s*?)<\/p>/g, '');
+
+		return parsedBody;
 	}
 
 	/**
@@ -45,11 +81,14 @@ class Story extends Component {
 							style={{ backgroundImage: `url(${story.image_url})` }}
 						></div>
 						<div className={styles.overlay}></div>
-						<h1 className={styles.title}>{story.title}</h1>
+						<div className={styles.header_wrapper}>
+							<p className={styles.author}>{story.author.name}</p>
+							<h1 className={styles.title}>{story.title}</h1>
+						</div>
 					</div>
 					<div
 						className={styles.body}
-						dangerouslySetInnerHTML={{ __html: story.body }}
+						dangerouslySetInnerHTML={{ __html: this.parseBody(story.body) }}
 					></div>
 				</div>
 			</div>
