c0a45ab9283531e85b0c43d1f87e33e26e549d6e
Robert van Steen <studio@rovansteen.nl>
2016-06-10 01:37:06 +0200

    Add input component.

---
+++ src/app/Components/Input/Component.jsx
@@ -0,0 +1,33 @@
+import _ from 'lodash';
+import React, { PropTypes } from 'react';
+import style from './style';
+
+/**
+ * Define the component.
+ */
+const Input = props => {
+	const inputProps = _.omit(props, ['label', 'touched', 'error']);
+
+	return (
+		<div className={style.wrapper}>
+			<label className={style.label}>{props.label}</label>
+			<input {...inputProps} className={style.input} />
+			<p className={style.error}>
+				{props.touched && props.error}
+			</p>
+		</div>
+	);
+};
+
+/**
+ * Define the proptypes of the component.
+ *
+ * @type {Object}
+ */
+Input.propTypes = {
+	label: PropTypes.string,
+	touched: PropTypes.bool,
+	error: PropTypes.string,
+};
+
+export default Input;
