ce9454ec41317e10e5ff57f428178ca37ddd0243
Robert van Steen <studio@rovansteen.nl>
2016-06-20 16:10:50 +0200

    Fix category source url

--- src/app/Sources/Categories.js
+++ src/app/Sources/Categories.js
@@ -2,7 +2,7 @@ import fetch from 'axios';
 import categories from 'Collections/Categories';
 
 export function fetchCategories() {
-	return fetch.get('categories')
+	return fetch.get('/categories')
 		.then(response =>
 				categories.addCollection(response.data.categories, false)
 		);
