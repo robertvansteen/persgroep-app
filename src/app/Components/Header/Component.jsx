49b8130b3a0ec9c35ceaae0fe89dc435b0806cdc
Robert van Steen <studio@rovansteen.nl>
2016-06-20 09:15:29 +0200

    Work in progress with the menu.

--- src/app/Components/Header/Component.jsx
+++ src/app/Components/Header/Component.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
 import styles from './style.css';
 import { Link } from 'react-router';
+import React, { PropTypes } from 'react';
 
-const Header = () => {
+const Header = props => {
 	return (
 		<div className={styles.header}>
 			<div className={styles.wrapper}>
+				<div className={styles.menu} onClick={props.onMenuClick}>
+					<i className="icon-menu"></i>
+				</div>
 				<Link to="/">
 					<h1 className={styles.title}>
 						Persgroep
@@ -14,6 +17,10 @@ const Header = () => {
 			</div>
 		</div>
 	);
+};
+
+Header.propTypes = {
+	onMenuClick: PropTypes.func.isRequired,
 };
 
 export default Header;
--- src/app/Sources/Auth.js
+++ src/app/Sources/Auth.js
@@ -3,3 +3,7 @@ import fetch from 'axios';
 export function refresh() {
 	return fetch.get('/auth/refresh');
 }
+
+export function fetchMe() {
+	return fetch.get('/me');
+}
