8e24a4e6524ad08189a2a0b126d719a8a0359f12
Robert van Steen <studio@rovansteen.nl>
2016-06-21 09:25:36 +0200

    Add spinner component.

---
+++ src/app/Components/Spinner/Component.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './style';
+
+const Spinner = () => {
+	return (
+		<div className={styles.block}>
+			<div className={styles.item}></div>
+			<div className={styles.item}></div>
+			<div className={styles.item}></div>
+			<div className={styles.item}></div>
+			<div className={styles.item}></div>
+			<div className={styles.item}></div>
+			<div className={styles.item}></div>
+			<div className={styles.item}></div>
+			<div className={styles.item}></div>
+		</div>
+	);
+}
+
+export default Spinner;
