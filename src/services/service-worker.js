139b563633d2f66b2d5504dc2d1769b6dde55324
Robert van Steen <studio@rovansteen.nl>
2016-07-05 22:34:10 +0200

    Improve caching for API & external resources

--- src/services/service-worker.js
+++ src/services/service-worker.js
@@ -1,27 +1,37 @@
-const { name, version } = self.__wpo;
-const cacheName = `${name}:${version}:api`;
+const API_CACHE = `API`;
+const EXTERNAL_CACHE = `EXTERNAL`;
 
 function isApiCall(url) {
 	return url.indexOf(process.env.API_ENDPOINT) >= 0;
 }
 
-function fetchAndCache(request) {
-	console.log('[SW]: Fetching & caching:', request.url);
+function isExternalResource(url) {
+	return url.indexOf(process.env.HOST) < 0;
+}
+
+function fetchAndCache(request, cacheName) {
+	console.log(`[SW]: Fetching & caching in ${cacheName}:`, request.url);
 	return fetch(request).then(response => {
 		caches.open(cacheName, response).then(cache => cache.put(request, response));
 		return response.clone();
 	});
 }
 
-function match(request) {
+function match(request, cacheName) {
 	return caches.match(request).then(response => {
 		if (response) return response.clone();
-		return fetchAndCache(request);
+		return fetchAndCache(request, cacheName);
 	});
 }
 
 self.addEventListener('fetch', event => {
-	if (!isApiCall(event.request.url)) return true;
+	if (isApiCall(event.request.url)) {
+		return event.respondWith(match(event.request, API_CACHE));
+	}
 
-	event.respondWith(match(event.request));
+	if (isExternalResource(event.request.url)) {
+		return event.respondWith(match(event.request, EXTERNAL_CACHE));
+	}
+
+	return event.respondWith(fetch(event.request));
 });
