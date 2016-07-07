38b6eb1054a3ee28543794b2f92418fb7e9fcbe4
Robert van Steen <studio@rovansteen.nl>
2016-07-07 02:34:40 +0200

    Set up service worker provider

---
+++ src/app/Providers/ServiceWorker.js
@@ -0,0 +1,26 @@
+
+class ServiceWorkerProvider {
+
+	/**
+	 * Register the service provder with the container.
+	 *
+	 * @param  {container} container
+	 * @return {void}
+	 */
+	register(container) {
+		container.registerProvider('service-worker', this);
+	}
+
+	/**
+	 * Boot up the service provider.
+	 *
+	 * @return {void}
+	 */
+	boot() {
+		if ('serviceWorker' in navigator) {
+			navigator.serviceWorker.register('/service-worker.js');
+		}
+	}
+}
+
+export default ServiceWorkerProvider;
